1b2085e4feb699a735537d4856d286bee1cb55b3
hellozin <paul26375@gmail.com>
2019-08-28 09:33:38 +0900

    durable 설정 변경

--- consumer.py
+++ consumer.py
@@ -10,7 +10,7 @@ channel.queue_declare(queue='post.create')
 def callback(ch, method, properties, body):
   print("[x] Received %r" % body)
 
-channel.basic_consume(queue='post.create', on_message_callback=callback, auto_ack=True)
+channel.basic_consume(queue='post.create', on_message_callback=callback, auto_ack=False)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
