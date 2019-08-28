2975f55dad3038898ca0daf301168335660f167a
hellozin <paul26375@gmail.com>
2019-08-28 18:39:51 +0900

    declare 제거

--- consumer.py
+++ consumer.py
@@ -5,12 +5,10 @@ credentials = pika.PlainCredentials(secret.username, secret.password)
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 5672, 'test', credentials))
 channel = connection.channel()
 
-channel.queue_declare(queue='post.create')
-
 def callback(ch, method, properties, body):
   print("[x] Received %r" % body)
 
-channel.basic_consume(queue='post.create', on_message_callback=callback, auto_ack=False)
+channel.basic_consume(queue='post.create', on_message_callback=callback, auto_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
